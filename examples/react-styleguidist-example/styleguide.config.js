7fd1e51d256d03c1eab1852bfcbb6e5e01a5f14c
Jacques Dukes <jdukes22@gmail.com>
2017-03-02 15:02:11 -0600

    Working StatelessComponents parsing

--- examples/react-styleguidist-example/styleguide.config.js
+++ examples/react-styleguidist-example/styleguide.config.js
@@ -2,29 +2,28 @@ const path = require('path');
 const glob = require('glob');
 
 module.exports = {
-	title: 'React Style Guide Example',
-	components: function() {
-		return glob.sync(path.resolve(__dirname, 'components/**/*.tsx'))
-			.filter(function(module) {
-				return /\/[A-Z]\w*\.tsx$/.test(module);
-			});
-	},
-	resolver: require('react-docgen').resolver.findAllComponentDefinitions,
-	
-	propsParser: require('../../lib/propTypesParser').parse,
-	
-	updateWebpackConfig: function(webpackConfig, env) {
-		const dir = path.resolve(__dirname, 'lib');
-        webpackConfig.resolve.extensions.push('.ts');
-        webpackConfig.resolve.extensions.push('.tsx');
-        
-        webpackConfig.module.loaders.push(           
-            { 
-                test: /\.tsx?$/,
-                include: __dirname,
-                loader: 'awesome-typescript-loader'
-            }
-        );
-		return webpackConfig;
-	}
+  title: 'React Style Guide Example',
+  components: function () {
+    return glob.sync(path.resolve(__dirname, 'components/**/*.tsx'))
+      .filter(function (module) {
+        return /\/[A-Z]\w*\.tsx$/.test(module);
+      });
+  },
+  resolver: require('react-docgen').resolver.findAllComponentDefinitions,
+
+  propsParser: require('../../lib/propTypesParser').parse,
+
+  updateWebpackConfig: function (webpackConfig, env) {
+    webpackConfig.resolve.extensions.push('.ts');
+    webpackConfig.resolve.extensions.push('.tsx');
+
+    webpackConfig.module.loaders.push(
+      {
+        test: /\.tsx?$/,
+        include: __dirname,
+        loader: 'ts-loader'
+      }
+    );
+    return webpackConfig;
+  }
 };
