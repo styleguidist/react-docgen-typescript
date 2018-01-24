6c4bec76fc615dfc99f4c4745482447b0b15608c
Havret <h4vret@gmail.com>
2018-01-24 22:07:45 +0100

    Apply prettier and lint to example project (#70)

--- examples/react-styleguidist-example/components/Grid.tsx
+++ examples/react-styleguidist-example/components/Grid.tsx
@@ -4,28 +4,30 @@ import * as React from 'react';
  * Grid properties.
  */
 export interface IGridProps {
-    /** prop1 description */
-    prop1?: string;
-    /** prop2 description */
-    prop2: number;
-    /**
-     * prop3 description
-     */
-    prop3: () => void;
-    /** Working grid description */
-    prop4: 'option1' | 'option2' | 'option3';
+  /** prop1 description */
+  prop1?: string;
+  /** prop2 description */
+  prop2: number;
+  /**
+   * prop3 description
+   */
+  prop3: () => void;
+  /** Working grid description */
+  prop4: 'option1' | 'option2' | 'option3';
 }
 
 /**
  * Form Grid.
  */
 export const Grid = (props: IGridProps) => {
-    const smaller = () => {return;};
-    return <div>Grid</div>;
+  const smaller = () => {
+    return;
+  };
+  return <div>Grid</div>;
 };
 
 const notExported = (props: IGridProps) => {
-    return <div>not exported grid</div>
+  return <div>not exported grid</div>;
 };
 
 export default Grid;
--- examples/react-styleguidist-example/components/Row.tsx
+++ examples/react-styleguidist-example/components/Row.tsx
@@ -5,26 +5,25 @@ import { Component } from 'react';
  * Row properties.
  */
 export interface IRowProps {
-    /** prop1 description */
-    prop1?: string;
-    /** prop2 description */
-    prop2: number;
-    /**
-     * prop3 description
-     */
-    prop3: () => void;
-    /** prop4 description */
-    prop4: 'option1' | 'option2' | 'option3';
+  /** prop1 description */
+  prop1?: string;
+  /** prop2 description */
+  prop2: number;
+  /**
+   * prop3 description
+   */
+  prop3: () => void;
+  /** prop4 description */
+  prop4: 'option1' | 'option2' | 'option3';
 }
 
 /**
  * Form Row.
  */
 export class Row extends Component<IRowProps, {}> {
-
-    render() {
-        return <div>Row</div>;
-    }
+  render() {
+    return <div>Row</div>;
+  }
 }
 
 export default Row;
