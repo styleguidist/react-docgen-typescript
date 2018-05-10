ad8679163c6c7d002cc8a5291cf9c796317f9c02
argshook <argshook@gmail.com>
2018-05-10 13:35:28 +0300

    chore(Parser): read displayName

--- src/__tests__/data/StatefulDisplayName.tsx
+++ src/__tests__/data/StatefulDisplayName.tsx
@@ -7,7 +7,7 @@ export interface StatefullProps {
 
 /** Statefull description */
 export class Statefull extends React.Component<StatefullProps> {
-  static displayName = 'i am stateless displayName';
+  static displayName = 'i am stateful displayName';
   render() {
     return <div>My Property = {this.props.myProp}</div>;
   }
