0bf3e89de8d4eb338d73738104e51001339acbd9
Miles Johnson <miles.johnson@airbnb.com>
2018-06-01 12:25:22 -0700

    Test intersection props.

---
+++ src/__tests__/data/HOCIntersectionProps.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import withHOC, { HOCInjectedProps } from './withHOC';
+
+export interface HOCProps {
+  /** myProp description */
+  myProp: string;
+}
+
+/** HOCIntersectionProps description */
+export const HOCIntersectionProps: React.SFC<
+  HOCProps & HOCInjectedProps
+> = props => <div />;
+
+export default withHOC({})(HOCIntersectionProps);
