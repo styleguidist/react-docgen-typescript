9ba350be219852397194600f803e368798c72a1b
strothj jason@jasons.io <jason@jasons.io>
2018-07-08 18:36:30 -0400

    Add additional exports to module entry point
    Added a few additional exports to the module's index to support use with
    react-docgen-typescript-loader. This allows this package to more easily
    be used as a peer dependency.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import {
   ComponentDoc,
+  FileParser,
   parse,
+  ParserOptions,
   PropItem,
   PropItemType,
   Props,
+  withCompilerOptions,
   withCustomConfig,
   withDefaultConfig
 } from './parser';
 
 export {
   parse,
+  withCompilerOptions,
   withDefaultConfig,
   withCustomConfig,
   ComponentDoc,
+  FileParser,
+  ParserOptions,
   Props,
   PropItem,
   PropItemType
