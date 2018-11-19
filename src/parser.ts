2dfb833b7373a4b41fa19e294f83786de16055c4
Dmitriy Kuznetsov <d.o.kuznetsov@tinkoff.ru>
2018-11-19 17:56:59 +0300

    Fix filename to js identifier conversion

--- src/parser.ts
+++ src/parser.ts
@@ -841,8 +841,13 @@ function computeComponentName(exp: ts.Symbol, source: ts.SourceFile) {
 // Default export for a file: named after file
 export function getDefaultExportForFile(source: ts.SourceFile) {
   const name = path.basename(source.fileName, path.extname(source.fileName));
+  const filename = name === 'index' ? path.basename(path.dirname(source.fileName)) : name;
 
-  return name === 'index' ? path.basename(path.dirname(source.fileName)) : name;
+  // JS identifiers must starts with a letter, and contain letters and/or numbers
+  // So, you could not take filename as is
+  const identifier = filename.replace(/[^A-Za-z]*|[^0-9.]*/g, '')
+
+  return identifier.length ? identifier : 'DefaultName'
 }
 
 function getParentType(prop: ts.Symbol): ParentType | undefined {
