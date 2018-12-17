b83c843dc49da59a095cd42d32b6c2324d951154
Pavel Vasek <pvasek@gmail.com>
2018-12-17 22:04:33 +0100

    example: styleguidist sections example added

---
+++ examples/react-styleguidist-sections-example/index.ts
@@ -0,0 +1,18 @@
+// this is fake app index file
+// just to make sure the webpack itself is setup correctly
+
+import { Column } from "./components/Column";
+import { ConstExportRow } from "./components/ConstExport";
+import { Grid } from "./components/Grid";
+import { HocComponent } from "./components/HocComponent";
+import { PureRow } from "./components/PureRow";
+import { Row } from "./components/Row";
+
+export {
+    Column,
+    ConstExportRow,
+    Grid,
+    HocComponent,
+    PureRow,
+    Row,
+}
---
+++ examples/react-styleguidist-sections-example/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+      bundle: ['./index.ts'],
+  },
+  context: path.resolve(__dirname),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'temp')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
