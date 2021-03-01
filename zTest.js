e9be68c9e7a55f6cc427ed664672ead823537284
carmnk <52402366+carmnk@users.noreply.github.com>
2021-03-01 22:44:20 +0100

    update fork to 1.21.0, added test to pr

--- zTest.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const docgen = require("./lib/parser");
-
-function fixturePath(componentName) {
-  return path.join(__dirname, "src", "__tests__", "data", `${componentName}.tsx`); // it's running in ./temp
-}
-
-const res = docgen.parse(fixturePath("FunctionalComponentsWithPropTags"), { shouldIncludePropTagMap: true, shouldExtractValuesFromUnion: true, shouldExtractLiteralValuesFromEnum: true });
-console.log(res);
