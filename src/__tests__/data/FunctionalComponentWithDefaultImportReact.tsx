07407006438e86916160f849ce9410b1344240bc
liuyuan <liuyuan.2004@bytedance.com>
2024-12-19 11:05:48 +0800

    feat: set esModuleInterop as default config

---
+++ src/__tests__/data/FunctionalComponentWithDefaultImportReact.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface JumbotronProps {
+  /** prop1 description */
+  prop1: string;
+}
+
+/**
+ * Jumbotron description
+ */
+export const Jumbotron: React.FC<JumbotronProps> = props => {
+  return <div>Test</div>;
+};
